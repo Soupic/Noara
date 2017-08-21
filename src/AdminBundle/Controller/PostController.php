f7b35f5af974702cc752f40909bfd50962660370
soupic <soupic@protonmail.com>
2017-08-21 17:17:18 +0000

    Methode showAction partie Post

--- src/AdminBundle/Controller/PostController.php
+++ src/AdminBundle/Controller/PostController.php
@@ -4,11 +4,26 @@ namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class PostController
+ * @package AdminBundle\Controller
+ */
 class PostController extends Controller
 {
+    /**
+     * @Route("posts/", name="show_posts")
+     * @return Response
+     */
     public function showAction()
     {
         $postDao = $this->get("noara.admin.dao.post");
+
+        $posts = $postDao->getAllPost();
+
+        return $this->render("AdminBundle:Post:liste.html.twig", [
+            "posts" => $posts,
+        ]);
     }
 }
--- src/AdminBundle/Service/DAO/PostDao.php
+++ src/AdminBundle/Service/DAO/PostDao.php
@@ -2,8 +2,43 @@
 
 namespace AdminBundle\Service\DAO;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManager;
+use AdminBundle\Exception\ArchitectureException;
 
-class PostDao
+
+/**
+ * Class PostDao
+ * @package AdminBundle\Service\DAO
+ */
+class PostDao extends AbstractMasterDAO
 {
-    
+    /**
+     * @return EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * @return ArrayCollection
+     * @throws ArchitectureException
+     */
+    public function getAllPost()
+    {
+        try {//On récupère le bon répository
+            $repo = $this->entity->getRepository("AdminBundle:Post");
+            //Appel à la méhtode du répo pour nous retourné tout les posts
+            $query = $repo->getAllPost();
+            //Nous retourne une tableau de collection avec le résulta
+            return new ArrayCollection($query->getResult());
+        } catch (\Exception $exception) {
+            throw new ArchitectureException(
+                "Erreur lors de la récupération des posts",
+                "TODO",
+                $exception
+            );
+        }
+    }
 }
