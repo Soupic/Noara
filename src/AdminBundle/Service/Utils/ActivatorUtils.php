ab234919bf6fd96c1ccc4111428911e70c3c0b4a
Soupic <pic.mayster@gmail.com>
2017-09-06 17:25:40 +0200

    Création des méthode d'activation, de validation et de désactivation

--- src/AdminBundle/Service/Utils/ActivatorUtils.php
+++ src/AdminBundle/Service/Utils/ActivatorUtils.php
@@ -70,11 +70,79 @@ class ActivatorUtils
         $this->characterPersistance = $characterPersistance;
     }
 
-    public function enable()
+    /**
+     * @param string $field
+     * @param int    $idField
+     */
+    public function enable($field, $idField)
     {
-        return $this->isValideForEnable(ActionEnum::KEY_POST);
+        $post = $field === ActionEnum::KEY_POST;
+        $race = $field === ActionEnum::KEY_RACE;
+        $character = $field === ActionEnum::KEY_CHAR;
+        //On vérifi si l'on peux passer l'élément à actif
+        if ($this->isValideForEnable($field)) {
+            if ($post) {
+                //On récupère le post sur lequel nous avons cliqué
+                //Doit appeler une méthode du DAO qui récupère le post
+                $post = $this->postDao->getPostById($idField);
+                //On lui passe alors actif de 0 à 1
+                $post->setActive(1);
+                //On persiste le post
+                $this->postPersistance->savePost($post);
+            } elseif ($race) {
+                $race = $this->raceDao->getRaceById($idField);
+
+                $race->setActive(1);
+
+                $this->racePersistance->saveRace($race);
+            } elseif ($character) {
+                $character = $this->characterDao->getRaceById($idField);
+
+                $character->setActive(1);
+
+                $this->characterPersistance->saveCharacters($character);
+            }
+        }
     }
 
+    /**
+     * @param string $field
+     * @param int    $idField
+     */
+    public function desabled($field, $idField)
+    {
+        $post = $field === ActionEnum::KEY_POST;
+        $race = $field === ActionEnum::KEY_RACE;
+        $character = $field === ActionEnum::KEY_CHAR;
+
+        if ($post) {
+            //On récupère le post sur lequel nous avons cliqué
+            //Doit appeler une méthode du DAO qui récupère le post
+            $post = $this->postDao->getPostById($idField);
+            //On lui passe alors actif de 0 à 1
+            $post->setActive(0);
+            //On persiste le post
+            $this->postPersistance->savePost($post);
+        } elseif ($race) {
+            $race = $this->raceDao->getRaceById($idField);
+
+            $race->setActive(0);
+
+            $this->racePersistance->saveRace($race);
+        } elseif ($character) {
+            $character = $this->characterDao->getRaceById($idField);
+
+            $character->setActive(0);
+
+            $this->characterPersistance->saveCharacters($character);
+        }
+
+    }
+
+    /**
+     * @param $field
+     * @return bool
+     */
     private function isValideForEnable($field)
     {
         $post = $field === ActionEnum::KEY_POST;
@@ -85,23 +153,42 @@ class ActivatorUtils
             //On appel la méthode pour récupéré tout les post actif
             $postEnables = $this->postDao->getCountEnablePoste(self::KEY_ENABLE);
             $countField = $postEnables;
+            //On compte le nombre d'élement actif pour les post et les personnages
+            $resultPostChar = count($countField);
 
         } elseif ($race) {
             //On appel la méthode pour récupéré tout les post actif
             $raceEnables = $this->raceDao->getCountEnableRace(self::KEY_ENABLE);
-            $countField = $raceEnables;
+            $countFieldRace = $raceEnables;
+            //Meme chose avec les races
+            $resultRace = count($countFieldRace);
+
 
         } elseif ($character) {
             //On appel la méthode pour récupéré tout les post actif
             $charactertEnables = $this->characterDao->getCountEnableCharacter(self::KEY_ENABLE);
             $countField = $charactertEnables;
+            //On compte le nombre d'élement actif pour les post et les personnages
+            $resultPostChar = count($countField);
 
         }
-        //On compte le nombre d'élement actif
-        $result = count($countField);
-        //Si le tableau est inférieur à 5 éléments on passe le parametre actif à 1
-        if ($result < 5) {
-            return true;
+//        //Si le tableau est inférieur à 5 éléments on passe le parametre actif à 1
+//        if (($resultPostChar < 5) || $resultRace < 4) {
+//            return true;
+//        }
+//
+//        return false;
+
+        if (isset($resultPostChar)) {
+            if ($resultPostChar < 5) {
+                return true;
+            }
+        }
+
+        if (isset($resultRace)) {
+            if ($resultRace < 4) {
+                return true;
+            }
         }
 
         return false;
