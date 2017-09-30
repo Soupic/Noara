dad8529c26a969ba8b81080ff1bb3de68c349db6
Soupic <pic.mayster@gmail.com>
2017-09-30 16:23:57 +0200

    Création du controlleur Race, pour la partie front

--- src/PageBundle/Controller/RacesController.php
+++ src/PageBundle/Controller/RacesController.php
@@ -4,6 +4,8 @@ namespace PageBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 
 /**
@@ -12,4 +14,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 class RacesController extends Controller
 {
+    const KEY_ENABLE = 1;
+
+    /**
+     * @Route("races/", name="show_races_json")
+     * @return Response
+     */
+    public function showRaceEnableAction()
+    {
+        $raceDao = $this->get("noara.dao.races");
+        $races = $raceDao->getCountEnableRace(self::KEY_ENABLE);
+//        dump($races);die();
+
+        foreach ($races as $race) {
+            $test[] = $race;
+        };
+
+        dump($test);
+        die();
+//        $datas = [
+//          "nom" => $races,
+//        ];
+
+        return new JsonResponse($test);
+//        return $this->render("PageBundle:Races:liste.html.twig", [
+//            "races" => $races,
+//        ]);
+    }
 }
