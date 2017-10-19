b19858765372202d233c39793c69dc4a2a527b9f
Soupic <pic.mayster@gmail.com>
2017-10-19 19:47:26 +0200

    Ajout de la page d'accueil

--- src/PageBundle/Controller/DefaultController.php
+++ src/PageBundle/Controller/DefaultController.php
@@ -7,12 +7,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-//    /**
-//     * @Route("/")
-//     */
-//    public function indexAction()
-//    {
-//        $test = $this->get
-//        return $this->render('PageBundle:Default:index.html.twig');
-//    }
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('PageBundle:Default:index.html.twig');
+    }
 }
